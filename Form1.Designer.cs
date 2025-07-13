9255d34a8196ee7bbe1c745ee8230f5b46a28345
Jahdel White <jahdelwhitetech@gmail.com>
2025-07-13 13:39:59 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace White_Ch8._9
+{
+    partial class CharToNum
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RunBtn = new System.Windows.Forms.Button();
+            this.ExitBtn = new System.Windows.Forms.Button();
+            this.UserInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ResultLbl = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // RunBtn
+            // 
+            this.RunBtn.Location = new System.Drawing.Point(110, 315);
+            this.RunBtn.Name = "RunBtn";
+            this.RunBtn.Size = new System.Drawing.Size(162, 60);
+            this.RunBtn.TabIndex = 0;
+            this.RunBtn.Text = "GET RESULT";
+            this.RunBtn.UseVisualStyleBackColor = true;
+            this.RunBtn.Click += new System.EventHandler(this.RunBtn_Click);
+            // 
+            // ExitBtn
+            // 
+            this.ExitBtn.Location = new System.Drawing.Point(581, 315);
+            this.ExitBtn.Name = "ExitBtn";
+            this.ExitBtn.Size = new System.Drawing.Size(162, 60);
+            this.ExitBtn.TabIndex = 1;
+            this.ExitBtn.Text = "EXIT";
+            this.ExitBtn.UseVisualStyleBackColor = true;
+            this.ExitBtn.Click += new System.EventHandler(this.ExitBtn_Click);
+            // 
+            // UserInput
+            // 
+            this.UserInput.Location = new System.Drawing.Point(255, 193);
+            this.UserInput.Name = "UserInput";
+            this.UserInput.Size = new System.Drawing.Size(240, 26);
+            this.UserInput.TabIndex = 2;
+            this.UserInput.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.UserInput_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(424, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(360, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter a phone number with LETTERS at the end: ";
+            // 
+            // ResultLbl
+            // 
+            this.ResultLbl.AutoSize = true;
+            this.ResultLbl.Location = new System.Drawing.Point(457, 335);
+            this.ResultLbl.Name = "ResultLbl";
+            this.ResultLbl.Size = new System.Drawing.Size(72, 20);
+            this.ResultLbl.TabIndex = 4;
+            this.ResultLbl.Text = "#######";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(546, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(228, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "For Example (843-GET-FOOD)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(644, 162);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "=";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(555, 193);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(203, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Coverts to:  (843-438-3663)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(290, 335);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(147, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "User # Conversion: ";
+            // 
+            // CharToNum
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ResultLbl);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.UserInput);
+            this.Controls.Add(this.ExitBtn);
+            this.Controls.Add(this.RunBtn);
+            this.Name = "CharToNum";
+            this.Text = "Letter to Number Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button RunBtn;
+        private System.Windows.Forms.Button ExitBtn;
+        private System.Windows.Forms.TextBox UserInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label ResultLbl;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace White_Ch8._9
+{
+    public partial class CharToNum : Form
+    {
+        public CharToNum()
+        {
+            InitializeComponent();
+            string UserGuess = UserInput.Text.Trim();
+        }
+
+        //method for ensuring the users input is correct
+        private void Validation(string str)
+        {
+            string UserGuess = UserInput.Text.Trim();
+            int StrLength = 14;
+            StrLength.ToString();
+            //User Guess must match the length of the how the format is showed on the app page
+            if (UserGuess.Length > StrLength)
+            {
+                MessageBox.Show("Your Number is too long enter less charaters");
+            }
+            bool valid = false;
+           
+            //if user does not have parenthesis and dashes the user is told it is invalid
+                if (UserGuess[0] == '(' && UserGuess[4] == '-' && UserGuess[8] == '-' && UserGuess[13] == ')')
+                {
+                    valid = true;
+                }
+                else
+                {
+                    MessageBox.Show("The number you have entered is invalid look to the format that is asked " +
+                        "for.");
+                }
+            //}
+            
+        }
+        private string Conversion(string str)
+        {
+
+            string UserGuess = UserInput.Text.Trim();
+            
+
+            int countVar = 0;
+            
+            //likely a problem with the < or you must enstate the length
+            for (countVar = 0; countVar < UserGuess.Length; countVar++)
+            {
+                //VERY INEFFECIENT APPROACH TALK TO PROFRESSOR ON HOW TO CONDENSE IT
+                //checks to see if users input at any point has these 1 letters
+
+                //countVar will represent each letter in user input
+                if (UserGuess[countVar] == 'A' || UserGuess[countVar] == 'B' || UserGuess[countVar] == 'C')
+                {
+                    //if the letters are found they are replaced with 2 
+                    UserGuess = UserGuess.Replace(UserGuess[countVar].ToString(), "2");
+                }
+                if (UserGuess[countVar] == 'D' || UserGuess[countVar] == 'E' || UserGuess[countVar] == 'F')
+                {
+                    //if the letters are found they are replaced with 3 
+                    UserGuess = UserGuess.Replace(UserGuess[countVar].ToString(), "3");
+                }
+                if (UserGuess[countVar] == 'G' || UserGuess[countVar] == 'H' || UserGuess[countVar] == 'I')
+                {
+                    //if the letters are found they are replaced with 4 
+                    UserGuess = UserGuess.Replace(UserGuess[countVar].ToString(), "4");
+                }
+                if (UserGuess[countVar] == 'J' || UserGuess[countVar] == 'K' || UserGuess[countVar] == 'L')
+                {
+                    //if the letters are found they are replaced with 5 
+                    UserGuess = UserGuess.Replace(UserGuess[countVar].ToString(), "5");
+                }
+                if (char.ToUpper(UserGuess[countVar]) == 'M' || (char.ToUpper(UserGuess[countVar]) == 'N' || char.ToUpper(UserGuess[countVar]) == 'O'))
+                {
+                    //if the letters are found they are replaced with 6
+                    UserGuess = UserGuess.Replace(UserGuess[countVar].ToString(), "6");
+                }
+                if (UserGuess[countVar] == 'P' || UserGuess[countVar] == 'Q' || UserGuess[countVar] == 'R' || UserGuess[countVar] == 'S')
+                {
+                    //if the letters are found they are replaced with 7
+                    UserGuess = UserGuess.Replace(UserGuess[countVar].ToString(), "7");
+                }
+                if (UserGuess[countVar] == 'T' || UserGuess[countVar] == 'U' || UserGuess[countVar] == 'V')
+                {
+                    //if the letters are found they are replaced with 8
+                    UserGuess = UserGuess.Replace(UserGuess[countVar].ToString(), "8");
+                }
+                if (UserGuess[countVar] == 'W' || UserGuess[countVar] == 'X' || UserGuess[countVar] == 'Y' || UserGuess[countVar] == 'Z')
+                {
+                    //if the letters are found they are replaced with 9
+                    UserGuess = UserGuess.Replace(UserGuess[countVar].ToString(), "9");
+                }
+
+
+
+            }
+            return UserGuess;
+
+
+
+
+
+        }
+
+        //ensure that the user can only enter symbols for parenthesis and dashes as well as letters and numbers
+        private void UserInput_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            if (!char.IsControl(e.KeyChar) && !char.IsLetterOrDigit(e.KeyChar)
+                && !char.IsSymbol(e.KeyChar) && !char.IsPunctuation(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        //Button runs program and all methods are called to get a final result
+        private void RunBtn_Click(object sender, EventArgs e)
+        {
+            string UserGuess = UserInput.Text.Trim();
+            Validation(UserGuess);
+            string first = UserGuess;
+            ResultLbl.Text = Conversion(first);
+            //int StrLength = UserGuess.Length;
+           // MessageBox.Show(StrLength.ToString());
+
+            
+            
+
+
+        }
+        //Exits the program upon clicking 
+        private void ExitBtn_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
